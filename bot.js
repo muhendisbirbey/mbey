3b868d3c130168b6f1d7ab449dc997ecab595d67
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2019-06-06 21:24:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,29 +23,6 @@ var oyun = [
         bot.user.setGame(oyun[random], "https://www.twitch.tv/muhendisbeymuhendishanim");
         }, 2 * 2500);  );
 
-module.exports = client => {
-var oyun = [
-        "YouTube > Mühendis Bey ile Mühendis Hanım ",
-        "Instagram > @muhendisbeyilemuhendishanim ",
-	"DLive.tv/muhendisbeyilhanim ",
-	"Mühendis BOT  " +client.guilds.size+ "  sunucuda  " + client.users.size +" kullanıcıya hizmet veriyor!",
-	"Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :)"
-    ];
-
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setGame(oyun[random], "https://www.twitch.tv/muhendisbeymuhendishanim" );
-        }, 2 * 2500);
-    
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-  client.user.setGame(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-};
 	
 bot.on('message', msg => {	
   if (msg.content === 's.a.' |msg.content === 's.a' |msg.content === 's' |msg.content === 'selm' |msg.content === 'salam' |msg.content === 'slm' |msg.content === 'sa' | msg.content ==='Sa'|msg.content ==='selamlar'|msg.content ==='sea'|msg.content ==='Sea' | msg.content ==='selam' | msg.content ==='Selamlar' | msg.content ==='Selam'){
